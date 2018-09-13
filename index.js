65b3d5b14a542dbb755225756389ae44f9ff39f1
Rachel <rnovick13@gmail.com>
2018-09-13 16:21:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// add solution here
+function theBeatlesPlay(musician, instruments) {
+  let music = [];
+ for (let i=0; i < musician.length; i++) {
+ music.push(musician[i] + ' plays ' + instruments[i]);
+}
+return music;
+} //complete
+
+function johnLennonFacts(johnLennonFacts) {
+  let facts = [];
+  while (johnLennonFacts < 4) {
+    for (let i=0; i < johnLennonFacts.length; i++) {
+    johnLennonFacts.push(johnLennonFacts[i] + '!!!');
+    }
+    return facts;
+  }
+} //come back to this one
+
+function iLoveTheBeatles() {
+  do {
+    return 'I love the Beatles!';
+    } while (i < 15);
+    }//complete
+   
+   function iLoveTheBeatles() {
+  do {
+    return 'I love the Beatles!';
+    } while (i < 15);
+   }
+   do {
+     return 'I love the Beatles!' 'I love the Beatles!' 'I love the Beatles!' 'I love the Beatles!' 'I love the Beatles!' 'I love the Beatles!' 'I love the Beatles!';
+   }
+     while (i < 7);
+     }
+     do { return 'I love the Beatles!';
+     }
+      while (i > 17);
+   }
